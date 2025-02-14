4344bc66c543c191aba799aceff0b7da5f6d0d8f
Abhishek <abhishekgoel@gofynd.com>
2025-02-14 22:57:33 +0530

    login route

--- bookMyShow-project/server/routes/user.js
+++ bookMyShow-project/server/routes/user.js
@@ -27,6 +27,34 @@ userRouter.post("/register", async (req, res) => {
         })
     }catch(err){
         console.log(err);
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+userRouter.post("/login", async (req, res) => {
+    try{
+        const {email,password: passwordFromClient} = req.body;
+        const user = UserModel.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message: "User does not exists. PLease register."
+            });
+        }
+        if(passwordFromClient !== user.password) {
+            return res.status(400).json({
+                message: "invalid credentials"
+            });
+        }
+        return res.status(200).json({
+            message: "User logged in succesfully"
+        })
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message: err.message
+        })
     }
 })
 
