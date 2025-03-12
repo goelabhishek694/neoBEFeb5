f17616652768e3ff5c531c708a5515d8f706f6ce
Abhishek <abhishekgoel@gofynd.com>
2025-03-12 23:43:49 +0530

    websockets

---
+++ websockets/websockets.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const {Server} = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("a user is connected", socket.id);
+    
+    //send a message from the server to client
+    socket.emit("message", "Welcome to the WS server !!");
+
+    //handle incoming messages from client
+    socket.on("client_message", (msg) => {
+        console.log("Message from client ", msg);
+        //Broadcast the message to all the connected client
+        io.emit("server_message", msg);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected ", socket.id);
+    })
+});
+
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+server.listen(3000, () => console.log("listening on port 3000"));
+
